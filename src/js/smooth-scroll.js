def238e2484087222dbe0fa1258b45fa03c651b6
Kelsie Nakasone <kkrnakasone@gmail.com>
2021-10-18 02:20:38 -0700

    Added animation to dynamic navbar. Added to About section and contact

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,9 +1,48 @@
-// document.getElementById("landing-scroll-button").onclick = function () {
-//   var mainSection = document.getElementById("main");
-//   var topPos = mainSection.offsetTop;
-//   window.scrollTo({ top: topPos, behavior: "smooth" });
-// };
+//Highlights the section in the nav bar as the user scrolls down the page
+const sections = document.querySelectorAll("section");
+const selectedSection = document.querySelector(".selected-section");
+const colors = ["red", "blue", "white"];
 
+//set to 70%
+const options = {
+  threshold: 0.7,
+};
+
+//looking for sections when they enter the frame
+let observer = new IntersectionObserver(navCheck, options);
+
+//checks when section comes in to view on the page
+function navCheck(entries) {
+  entries.forEach((entry) => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const coords = activeAnchor.getBoundingClientRect();
+    console.log(coords);
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left,
+    };
+    if (entry.isIntersecting) {
+      selectedSection.style.setProperty("left", `${directions.left}px`);
+      selectedSection.style.setProperty("top", `${directions.top}px`);
+      selectedSection.style.setProperty("width", `${directions.width}px`);
+      selectedSection.style.setProperty("height", `${directions.height}px`);
+      console.log(className);
+      // document.getElementsById(className).scrollIntoView({
+      //   behavior: "smooth",
+      // });
+    }
+  });
+}
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+//Smooth scrolls to the selected section when user selects from Navbar
+//also when user select "scroll down" on landing page
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -14,10 +53,27 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// function smoothScrollTo(buttonID, sectionID) {
-//   document.getElementById(buttonID).onclick = function () {
-//     var sectionElem = document.getElementById(sectionID);
-//     var topPos = sectionElem.offsetTop + 6;
-//     window.scrollTo({ top: topPos, behavior: "smooth" });
-//   };
+// $(window).scroll(example);
+
+// function example() {
+//   var tempScrollTop = $(window).scrollTop();
+//   console.log("Scroll from Top: " + tempScrollTop.toString());
+
+//   mainSection = document.getElementById("main");
+//   if (tempScrollTop > 1000 && tempScrollTop < 1050) {
+//     console.log("transition");
+//     scrollTo(document.documentElement, mainSection.offsetTop, 800);
+//   }
+// }
+
+// function scrollTo(element, to, duration) {
+//   if (duration <= 0) return;
+//   var difference = to - element.scrollTop;
+//   var perTick = (difference / duration) * 10;
+
+//   setTimeout(function () {
+//     element.scrollTop = element.scrollTop + perTick;
+//     if (element.scrollTop === to) return;
+//     scrollTo(element, to, duration - 10);
+//   }, 10);
 // }
