a0f28212a5a9e4175ce4c5c76f39efd69a9cdc95
Kelsie Nakasone <kkrnakasone@gmail.com>
2021-11-07 15:23:57 -0800

    Finished styling resume page and resume content. Completed all styling

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,20 +1,21 @@
 //when you click the mobile menu toggle, this function will run and reveal the navbar
 const toggle = document.querySelector(".hamburger-toggle");
-const leftDropDown = document.querySelector(".left-nav-container");
+const leftDropDown = document.querySelector("div.left-nav-container");
 const rightDropDown = document.querySelector(".right-nav-container");
 const toggleTop = document.querySelector(".top");
 const toggleMiddle = document.querySelector(".middle");
 const toggleBottom = document.querySelector(".bottom");
 
 toggle.addEventListener("click", function () {
-  if (leftDropDown.style.display == "none") {
+  console.log(leftDropDown.currentStyle);
+  if (leftDropDown.style.display != "none") {
+    closeMobileMenu();
+  } else {
     leftDropDown.style.display = "flex";
     rightDropDown.style.display = "flex";
     toggleTop.style.transform = "translateY(9px) rotateZ(45deg)";
     toggleMiddle.style.opacity = "0";
     toggleBottom.style.transform = "translateY(-9px) rotateZ(-45deg)";
-  } else {
-    closeMobileMenu();
   }
 });
 
